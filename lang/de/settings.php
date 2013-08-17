d2b64e43faa2b56650950ffa77b4dd6f094b1cec
Lars Pötter <lists@ing-poetter.de>
2013-08-17 17:30:05 +0200

    translation update

---
+++ lang/de/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Lars Pötter <lists@ing-poetter.de>
+ */
+$lang['email']                 = 'Bitte geben Sie hier die eMail Adresse ein an die sich Benutzer wenden können wenn sie unabsichtlich blockiert worden sind.';
+$lang['skipblackhole']         = 'Dieses Plugin bezieht zu blockierende IP Adressen von anderen Schwarzen Listen. Dadurch kann es sein das unschuldige Nutzer gebnlockt werden wenn ihnen die IP Adresse von der Angriffe ausgingen dynamisch zugewiesen wird. Wenn Sie diese option aktivieren werden keien IP Adressen mehr von anderen schwarzen Listen herangezogen.';
