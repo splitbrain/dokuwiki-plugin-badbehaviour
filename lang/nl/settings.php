b3e42e3b943fe5da5b0ae70b3ee898b02acb8a08
Hugo Smet <hugo.smet@scarlet.be>
2013-08-20 02:10:07 +0200

    translation update

---
+++ lang/nl/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hugo Smet <hugo.smet@scarlet.be>
+ */
+$lang['email']                 = 'Geef een email adres op waar gebruikers hulp kunnen vragen wanneer ze ongewild geblokkeerd worden.';
+$lang['skipblackhole']         = 'Bad Behavior checkt bij verstek bepaalde realtime blacklists voor gekende malafide IPs. Dit kan leiden tot blokkering van onschuldige gebruikers wiens dynamisch IP adres misbruikt is. Activeer deze optie om de blackhole check over te slaan.';
