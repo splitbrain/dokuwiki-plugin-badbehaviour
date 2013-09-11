44f61be82cb7328730c2834fba8957e2136f8f16
Hideaki SAWADA <chuno@live.jp>
2013-09-11 18:00:07 +0200

    translation update

---
+++ lang/ja/lang.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['menu']                  = 'Bad Behaviour 統計';
+$lang['percent']               = 'パーセント';
+$lang['count']                 = '件数';
+$lang['reason']                = '理由';
+$lang['blocked']               = '%d 件のアクセス（ %d 日間で）をブロックしました。';
+$lang['lkpresult']             = 'IP <b>%s</b> からの要求は <b>%s</b> を理由にブロックしました。ユーザーには <b>%s</b> という説明が表示されました。<a href="http://www.ioerror.us/bb2-support-key?key=%s">ここ</a>に詳細があります。';
+$lang['lkplist']               = '%d 日間でこの IP に一致したログの一覧は以下のとおりです。';
+$lang['lookup']                = 'サポートキーの検索';
