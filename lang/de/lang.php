8ce6e8d9153d3f49efdb2d20bd7bfc25be77901f
Christian <chris@m00n.eu>
2013-10-24 09:35:37 +0200

    translation update

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -4,6 +4,7 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Lars Pötter <lists@ing-poetter.de>
+ * @author Christian <chris@m00n.eu>
  */
 $lang['menu']                  = 'Missbrauchs Statistik';
 $lang['percent']               = 'Prozent';
@@ -11,3 +12,4 @@ $lang['count']                 = 'Anzahl';
 $lang['reason']                = 'Grund';
 $lang['blocked']               = '%d Zugruffe wurden in den letzen %d tagen blockiert.';
 $lang['lkpresult']             = 'Die Anfrage kam von der IP Adresse <b>%s</b> und wiurde blcokiert, auf grund von <b>%s</b>. Die Erklärung die dem Benutzer angezeigt wurde war <b>%s</b> Weitere details könnten <a href="http://www.ioerror.us/bb2-support-key?key=%s">hier</a> verfügbar sein.';
+$lang['lkplist']               = 'Folgende Log-Einträge der letzten $d Tage stimmen mit dieser IP überein.';
