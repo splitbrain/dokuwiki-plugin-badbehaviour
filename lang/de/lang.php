142ab6958d62424015dcf53ba4b9e4c8142a0c1f
Egmont Schreiter <egmont.schreiter@gmx.de>
2013-10-29 08:30:07 +0100

    translation update

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -4,10 +4,13 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Lars Pötter <lists@ing-poetter.de>
+ * @author Egmont Schreiter <egmont.schreiter@gmx.de>
  */
 $lang['menu']                  = 'Missbrauchs Statistik';
 $lang['percent']               = 'Prozent';
 $lang['count']                 = 'Anzahl';
 $lang['reason']                = 'Grund';
-$lang['blocked']               = '%d Zugruffe wurden in den letzen %d tagen blockiert.';
-$lang['lkpresult']             = 'Die Anfrage kam von der IP Adresse <b>%s</b> und wiurde blcokiert, auf grund von <b>%s</b>. Die Erklärung die dem Benutzer angezeigt wurde war <b>%s</b> Weitere details könnten <a href="http://www.ioerror.us/bb2-support-key?key=%s">hier</a> verfügbar sein.';
+$lang['blocked']               = '%d Zugriffe wurden in den letzen %d tagen blockiert.';
+$lang['lkpresult']             = 'Die Anfrage kam von der IP Adresse <b>%s</b> und wurde blockiert, aufgrund von <b>%s</b>. Die Erklärung die dem Benutzer angezeigt wurde war <b>%s</b>. Weitere Details könnten <a href="http://www.ioerror.us/bb2-support-key?key=%s">hier</a> verfügbar sein.';
+$lang['lkplist']               = 'Hier ist eine Liste von Log-Zeilen mit dieser IP in den letzten %d Tagen.';
+$lang['lookup']                = 'Der support key.';
--- lang/de/settings.php
+++ lang/de/settings.php
@@ -4,6 +4,7 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Lars Pötter <lists@ing-poetter.de>
+ * @author Egmont Schreiter <egmont.schreiter@gmx.de>
  */
 $lang['email']                 = 'Bitte geben Sie hier die eMail Adresse ein an die sich Benutzer wenden können wenn sie unabsichtlich blockiert worden sind.';
-$lang['skipblackhole']         = 'Dieses Plugin bezieht zu blockierende IP Adressen von anderen Schwarzen Listen. Dadurch kann es sein das unschuldige Nutzer gebnlockt werden wenn ihnen die IP Adresse von der Angriffe ausgingen dynamisch zugewiesen wird. Wenn Sie diese option aktivieren werden keien IP Adressen mehr von anderen schwarzen Listen herangezogen.';
+$lang['skipblackhole']         = 'Dieses Plugin bezieht zu blockierende IP Adressen von anderen Schwarzen Listen. Dadurch kann es sein das unschuldige Nutzer geblockt werden wenn ihnen die IP Adresse von der Angriffe ausgingen dynamisch zugewiesen wird. Wenn Sie diese option aktivieren werden keinen IP Adressen mehr von anderen schwarzen Listen herangezogen.';
