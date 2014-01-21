2f170e889337333d9be27668c21501b08f09c4f0
Schplurtz le Déboulonné <schplurtz@laposte.net>
2014-01-21 15:10:14 +0100

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
+ */
+$lang['menu']                  = 'Statistiques des mauvais comportements';
+$lang['percent']               = 'Pourcentage';
+$lang['count']                 = 'Quantité';
+$lang['reason']                = 'Raison';
+$lang['blocked']               = '%d accès bloqués dans les %d derniers jours.';
+$lang['lkpresult']             = 'La requête de l\'IP <b>%s<b> a été bloquée à cause de <b>%s</b>. L\'explication fournie à l\'utilisateur était <b>%s</b>. Pour de plus amples détails, consulter <a href="http://www.ioerror.us/bb2-support-key?key=%s">cette page</a>.';
+$lang['lkplist']               = 'Ci dessous, une liste de ligne du journal concernant cette IP pour les %d derniers jours.';
