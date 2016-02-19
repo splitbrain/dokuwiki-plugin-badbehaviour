1bbaa9e64a52f0100281fdcefe5867f423db6888
Morgan <morgan.leguen@gmail.com>
2016-02-19 14:20:07 +0100

    translation update

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -4,6 +4,7 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
+ * @author Morgan <morgan.leguen@gmail.com>
  */
 $lang['email']                 = 'Veuiller fournir une adresse de courriel à laquelle les utilisateurs malencontreusement bloqués peuvent demander de l\'aide.';
-$lang['skipblackhole']         = 'Par défaut, cette extension compulse certaines liste noires en temps réel à la recherche d\'adresses IP malveillantes. Ceci peut bloquer un utilisateur innocent qui obtient une adresse IP dynamique ayant précédemment sévi. Activer cette option pour ignorer la recherche dans les listes noires.';
+$lang['skipblackhole']         = 'Par défaut, cette extension vérifie certaines listes noires en temps réel à la recherche d\'adresses IP malveillantes. Ceci peut bloquer un utilisateur innocent qui obtient une adresse IP dynamique ayant précédemment sévi. Activer cette option pour ignorer la recherche dans les listes noires.';
